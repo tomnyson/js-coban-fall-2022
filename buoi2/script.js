84319db9daecbc79448895cf879a7d0c94c3fbb2
tomnyson <tabletkindfire@gmail.com>
2022-11-15 14:01:06 +0700

    bai 2 object json

--- buoi2/script.js
+++ buoi2/script.js
@@ -1,161 +1,120 @@
-// console.log("hello world");
-// var a = 1;
-// a = 10;
-// console.log(a);
+const mang = [1, 2, 8, 4, 6, 7, 3, 0, 9];
 
-// //hằng số
-// // const b = 1;
-// // b = 2;
-// // console.log(b);
+//duyet mang
+let min = mang[0];
+let sum = 0;
+let dem = 0;
+for (let i = 0; i < mang.length; i++) {
+  //cách gọi hàm
+  if (KTChan(mang[i])) {
+    console.log(`so chan ${mang[i]}`);
+  } else {
+    console.log(`so le ${mang[i]}`);
+  }
+  if (min > mang[i]) {
+    min = mang[i];
+  }
+  sum = sum + i;
+  dem++;
+  console.log(`mang tai vi tri ${i} gia tri: ${mang[i]}`);
+}
+console.log(`min ${min}`);
+console.log(`tong: ${sum}`);
+console.log(`tong tbc: ${sum / dem}`);
 
-// const ten = "le hong son";
-// const tuoi = 30;
-// const chieucao = 1.75;
+/**
+ *   in gia tri chan le
+ *  tim gia tri lon nhat cua mang
+ *  tim gia tri nho nhat
+ *  tinh tong cua mang
+ *  tinh trung binh cong cua mang
+ */
 
-// console.log("ten:", ten);
-// console.log("tuoi", tuoi);
-// console.log("chieu cao", chieucao);
+function KTChan(x) {
+  if (x % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// OBJECT
+const sinhvien = {
+  ten: "Lê Hồng Sơn",
+  tuoi: 30,
+  nganh: "CNTT",
+};
 
-// const diem = 2.5;
-// // true || false
-// if (diem >= 5) {
-//   console.log("pass môn");
-// } else {
-//   console.log("học lại");
-// }
-// // if else
-// const soThu1 = 9;
-// const soThu2 = 9;
+console.log(sinhvien);
+console.log(sinhvien["ten"]);
+console.log(sinhvien.ten);
+// object string
+console.log(JSON.stringify(sinhvien));
+console.log(JSON.parse(JSON.stringify(sinhvien)));
 
-// if (soThu1 >= soThu2) {
-//   console.log(`số thứ 1 ${soThu1} >= ${soThu2} số thứ 2`);
-// } else {
-//   console.log(`số thứ 1 ${soThu1} <= ${soThu2} số thứ 2`);
-// }
+const sinhvienList = [
+  {
+    id: 1,
+    ten: "a",
+    tuoi: 30,
+    nganh: "CNTT",
+  },
+  {
+    id: 2,
+    ten: "b",
+    tuoi: 30,
+    nganh: "LTMT",
+  },
+];
 
-// // if else if else
+console.log(sinhvienList);
+/**
+ * duyet mang
+ *  tìm kiếm
+ * thêm
+ * xoá
+ * sửa
+ */
+for (let i = 0; i < sinhvienList.length; i++) {
+  console.log(sinhvienList[i]);
+}
+console.log("bai 2");
+for (let i = 0; i < sinhvienList.length; i++) {
+  if (sinhvienList[i].id == 21) {
+    console.log(sinhvienList[i]);
+  }
+}
+console.log("bai 3");
+const sinhvienMoi = {
+  id: 3,
+  ten: "c",
+  tuoi: 20,
+  nganh: "TKW",
+};
+// them
+sinhvienList.push(sinhvienMoi);
+for (let i = 0; i < sinhvienList.length; i++) {
+  console.log(sinhvienList[i]);
+}
+//cap nhat id = 3 -> tuoi = 20 -> 21
 
-// if (soThu1 == soThu2) {
-//   console.log(`hai số đều bằng ${soThu2}`);
-// } else if (soThu1 > soThu2) {
-//   console.log(`số thứ 1 ${soThu1} > ${soThu2} số thứ 2`);
-// } else {
-//   console.log(`số thứ 1 ${soThu1} < ${soThu2} số thứ 2`);
-// }
-// /**
-//  * if -> else if -> else
-//  * nếu >=5 tb >=6.5 khá >= 8.0 giỏi >= 9.0 xuất sắc còn lại thì sẽ yếu
-//  */
+for (let i = 0; i < sinhvienList.length; i++) {
+  if (sinhvienList[i].id == 3) {
+    sinhvienList[i].tuoi = 21;
+  }
+}
 
-// const diemTb = 9.5;
+for (let i = 0; i < sinhvienList.length; i++) {
+  console.log(sinhvienList[i]);
+}
 
-// if (diemTb >= 9) {
-//   console.log("xuất sắc");
-// } else if (diemTb >= 8) {
-//   console.log("giỏi");
-// } else if (diemTb >= 6.5) {
-//   console.log("khá");
-// } else if (diemTb >= 5) {
-//   console.log("trung bình");
-// } else {
-//   console.log("yếu");
-// }
+// xoa id =2
 
-// const docSo = 10;
+for (let i = 0; i < sinhvienList.length; i++) {
+  if (sinhvienList[i].id == 2) {
+    delete sinhvienList[i];
+  }
+}
 
-// switch (docSo) {
-//   case 0:
-//     console.log("số 0");
-//     break;
-//   case 1:
-//     console.log("số 1");
-//     break;
-//   case 2:
-//     console.log("số 2");
-//     break;
-//   case 3:
-//     console.log("số 3");
-//     break;
-//   case 4:
-//     console.log("số 4");
-//     break;
-//   case 5:
-//     console.log("số 5");
-//     break;
-//   default:
-//     console.log("error");
-//     break;
-// }
-// // Tháng 1 có 31 ngày
-// // Tháng 2 có 28 ngày hoặc 29 ngày
-// // Tháng 3 có 31 ngày
-// // Tháng 4 có 3 ngày
-// // Có thể có 31 ngày
-// // Tháng 6 có 30 ngày
-// // Tháng 7 có 31 ngày
-// // Tháng 8 có 31 ngày
-// // Tháng 9 có 30 ngày
-// // Tháng 10 có 31 ngày
-// // Tháng 11 có 30 ngày
-// // Tháng 12 có 31 ngày
-// const thang = 2;
-// switch (thang) {
-//   case 1:
-//   case 3:
-//   case 5:
-//   case 10:
-//   case 12:
-//     console.log("31 ngay");
-//     break;
-//   case 6:
-//   case 9:
-//   case 11:
-//     console.log("30 ngay");
-//     break;
-//   case 2:
-//     console.log("28 or 29(năm nhuận) ngay");
-//     break;
-//   default:
-//     console.log("ko hop le");
-//     break;
-// }
-
-// // for
-// /**
-//  * khởi tạo ban đầu
-//  * điều kiện dừng
-//  * bước nhảy
-//  */
-// for (let i = 0; i < 10; i += 1) {
-//   console.log(i);
-// }
-// //b1: in chan n=10
-// console.log("bai 1 for");
-// for (let i = 0; i < 10; i += 1) {
-//   if (i % 2 == 0) console.log(i);
-// }
-// console.log("bai 1 while");
-// let i = 0;
-// while (i < 10) {
-//   if (i % 2 == 0) console.log(i);
-//   i++;
-// }
-// console.log("bai 1 do while");
-// i = 0;
-// do {
-//   if (i % 2 == 0) console.log(i);
-//   i++;
-// } while (i < 10);
-// // b2 in chan va(&&) chia het cho 5 n=50
-// console.log("bai 2");
-// for (let i = 0; i < 50; i += 1) {
-//   if (i % 2 == 0 && i % 5 == 0) console.log(i);
-// }
-// console.log("bai 3");
-// // b4 in le ||(or) chia het cho 3 n=50
-// for (let i = 0; i < 50; i += 1) {
-//   if (i % 2 != 0 || i % 5 == 0) console.log(i);
-// }
-//mảng
-const mang = [1, 2, 3, 4, 5, 7, 8, 9];
-console.log(mang);
+for (let i = 0; i < sinhvienList.length; i++) {
+  console.log(sinhvienList[i]);
+}
